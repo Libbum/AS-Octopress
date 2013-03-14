2c1d38aa4a2001752403182ac092e90f385f9439
Tim DuBois <genesis@neophilus.net>
2013-03-14 23:09:34 +1100

    Completed search. Updated config to reflect subdomain in atom

--- .themes/axiom/source/javascripts/jquery-tapir.js
+++ .themes/axiom/source/javascripts/jquery-tapir.js
@@ -3,8 +3,12 @@
   var settings = {};
   var methods = {
     init: function(options) {
-      el = this;
+      el = '<div id="content"><div class="article_t"></div><article role="article"><header><h1 class="entry-title">Search Results</header>';
       
+      jQuery('#load').remove();
+      jQuery('div#main').append('<span id="load">LOADING...</span>');
+      jQuery('#load').fadeIn('fast');
+
       settings = {
                    token: false,
                    query_param: ''
@@ -17,48 +21,36 @@
         return this;
       }
       var jqxhr = jQuery.getJSON('http://tapirgo.com/api/1/search.json?token=' + settings.token + '&query=' + settings.query_param + '&callback=?', function(data){
-       /* WORKING! jQuery.each(data, function(index) {
-                      alert('title: ' + data[index].title);
-                                          });*/
           jQuery.each(data, function(key, val) {
-/*            if(settings['complete']) { settings.complete() } */
-            alert('title: ' + val.title);
             var str1 = val.content;
             var str2 = str1.substr(1, 250);
             str2 = str2.substr(0, Math.min(str2.length, str2.lastIndexOf(" ")));
-            el.replaceWith('<div id="content" class="inner"><article class="post"><h2 class="title"><a href="' + val.link + '">' + val.title + '</a></h2><div class="entry-content"><div class="date">Published on: ' + (val.published_on).substr(0,10) + '</div></div><br><p>' + str2 + '...</p><br><a href="' + val.link +'">Read on &rarr;</a></article></div>');
-/*            if(key >= (data.length-1)){
-                el.append('<div id="content" class="inner"><center><h2 style="border-bottom: 1px solid #ddd;">Result ends</h2></center><br><br></div>');
-            } */
+            
+            el += '<div class="blueline" style="margin-top: 20px;"></div><h2 style="background: none;"><a href="' + val.link + '">' + val.title + '</a></h2><div class="entry-content"><div class="date">Published on: ' + (val.published_on).substr(0,10) + '</div></div><br><p>' + str2 + ' . . .</p><br><a href="' + val.link +'">Read on &rarr;</a>';
           });
+          el += '</article><div class="article_b"></div>';
+          showContent(el);
         })
-     .success(function() {
-            alert("second success"); 
-       })
+//     .success(function() { alert("second success"); })
      .error(function(jqXHR, textStatus, errorThrown) {
                console.log("error " + textStatus);
                        console.log("incoming Text " + jqXHR.responseText);
                            })
-     .complete(function() { alert("complete"); });
-      /*jQuery.getJSON(
-        'http://tapirgo.com/api/1/search.json?token=' + settings.token + '&query=' + settings.query_param, 
-        function(data){
-          if(settings['complete']) { settings.complete() }
-          jQuery.each(data, function(key, val) {
-          document.getElementById('toLoad').style.display="block";
-          document.getElementById('toLoad').style.height="100%";
-          document.getElementById('toLoad').style.overflow="hidden";
-              var str1 = val.content;
-              var str2 = str1.substr(1, 250);
-              str2 = str2.substr(0, Math.min(str2.length, str2.lastIndexOf(" ")));
-            el.append('<div id="content" class="inner"><article class="post"><h2 class="title"><a href="' + val.link + '">' + val.title + '</a></h2><div class="entry-content"><div class="date">Published on: ' + (val.published_on).substr(0,10) + '</div></div><br><p>' + str2 + '...</p><br><a href="' + val.link +'">Read on &rarr;</a></article></div>');
-            if(key >= (data.length-1)){
-                el.append('<div id="content" class="inner"><center><h2 style="border-bottom: 1px solid #ddd;">Result ends</h2></center><br><br></div>');
-            }
-          });
-        }
-      );*/
-      return this;
+//     .complete(function() { /*jQuery('#load').fadeOut('normal');*/ });
+      
+      function showContent(toLoad) {
+        jQuery('#toLoad').fadeTo('fast',0.00001,loadContent);  
+        function loadContent() {
+          jQuery('#toLoad').html(toLoad,showNewContent());
+        }   
+        function showNewContent() {
+          jQuery('#toLoad').delay(500).fadeTo('normal',1,hideLoader());
+        }   
+        function hideLoader() {
+         jQuery('#load').fadeOut('normal');
+        }   
+      }
+    return false;
     }
   };
 
