13176fc42437bbe7e21220e79e726826f6697359
Tim DuBois <genesis@neophilus.net>
2013-03-17 16:46:25 +1100

    Fixed MathJax to work with jQuery .load events

--- source/javascripts/ajaxify.js
+++ source/javascripts/ajaxify.js
@@ -9,54 +9,27 @@ jQuery(document).ready(function() {
 		}						
 	});
 
-	jQuery('li#ajax a').click(function(){
-		var toLoad = jQuery(this).attr('href')+' #toLoad';
-		jQuery('#toLoad').fadeTo('fast',0.00001,loadContent);
-		jQuery('#load').remove();
-		jQuery('div#main').append('<span id="load">LOADING...</span>');
-		jQuery('#load').fadeIn('fast');
-		//Keep a hash for those browsers that push will fuck up on
-		window.location.hash = jQuery(this).attr('href').substr(0,jQuery(this).attr('href').length-5);
-    //push url up.
-    history.pushState(null, null, jQuery(this).attr('href'));
-    jQuery('title').html(jQuery(this).attr('alt'));
-    function loadContent() {
-			jQuery('#toLoad').load(toLoad,showNewContent())
-		}
-		function showNewContent() {
-			jQuery('#toLoad').delay(500).fadeTo('normal',1,hideLoader());
-		}
-		function hideLoader() {
-			jQuery('#load').fadeOut('normal');
-		}
-		return false;
+	jQuery("li#ajax a").click(function(){
+	  var toLoad = jQuery(this).attr("href")+" #content";
+    jQuery("#toLoad").fadeTo("fast",0.00001,function(){ 
+        jQuery("#toLoad").load(toLoad, function(){
+            jQuery("#toLoad").delay(500).fadeTo("normal",1, function(){ jQuery("#load").fadeOut("normal"); });
+            MathJax.Hub.Queue(["Typeset",MathJax.Hub,"toLoad"]);
+         })
+     });
+     jQuery("#load").remove();
+     jQuery("#main").append("<span id=\"load\">LOADING...</span>");
+     jQuery("#load").fadeIn("fast");
+
+		 //Keep a hash for those browsers that push will fuck up on
+		 window.location.hash = jQuery(this).attr('href').substr(0,jQuery(this).attr('href').length-5);
+     //push url up.
+     history.pushState(null, null, jQuery(this).attr('href'));
+     jQuery('title').html(jQuery(this).attr('alt'));
+    
+    return false;
 		
 	});
-
-	jQuery('a.ajax').click(function(){
-		
-		var toLoad = jQuery(this).attr('href')+' #toLoad';
-		jQuery('#toLoad').fadeTo('fast',0.00001,loadContent);
-		jQuery('#load').remove();
-		jQuery('div#main').append('<span id="load">LOADING...</span>');
-		jQuery('#load').fadeIn('fast');
-		//Keep a hash for those browsers that push will fuck up on
-    window.location.hash = jQuery(this).attr('href').substr(0,jQuery(this).attr('href').length-5);
-    //push url up.
-    history.pushState(null, null, jQuery(this).attr('href'));
-    jQuery('title').html(jQuery(this).attr('alt'));
-    function loadContent() {
-			jQuery('#toLoad').load(toLoad,showNewContent())
-		}
-		function showNewContent() {
-			jQuery('#toLoad').delay(500).fadeTo('normal',1,hideLoader());
-		}
-		function hideLoader() {
-			jQuery('#load').fadeOut('normal');
-		}
-		return false;
-	});
-
 
 });
 
